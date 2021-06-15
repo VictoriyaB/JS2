4fbae79f49e02419e1076868cecabc68a26faa1b
Victoriya <victoriya.brovkina@gmail.com>
2021-06-16 00:17:19 +0400

    Lesson3

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ class ProductsList{
                 this.goods = [...data];
                 this.render();
             });
+        this.totalCostProducts();
     }
 
     _getProducts() {
@@ -25,6 +26,7 @@ class ProductsList{
             const productObj = new ProductItem(product);
             block.insertAdjacentHTML('beforeend', productObj.render());
         }
+        
     }
 
     totalCostProducts() {
@@ -51,20 +53,74 @@ class ProductItem {
 }
 
 class CartList{
-    addToCart() {} // создает новый объект класса CartItem, вызывает метод render у этого объекта
+    constructor(container = '.cart') {
+    this.container = container;
+    this.goods = [];
+    this._getCartItems()
+        .then(data => {
+            this.goods = [...data.contents];
+            this.render();
+        });
+    this.showCart();    
+    }
+
+    _getCartItems() {
+        return fetch(`${API}/getBasket.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    render() {
+        const block = document.querySelector(this.container);
+        for(let item of this.goods) {
+            const itemObj = new CartItem(item);
+            block.insertAdjacentHTML('beforeend', itemObj.render());
+        }
+    
+    }
+
+    showCart() { // корзина выпадает при нажатии на кнопку корзина
+        document.querySelector('.btn-cart').addEventListener('click', () => {
+            document.querySelector(this.container).classList.toggle('hidden');
+        });
+    } 
+
+    addToCart() { // создает новый объект класса CartItem, вызывает метод render у этого объекта
+        window.onload = () => {
+            document.getElementsByClassName('buy-btn').addEventListener('click', () => {
+                let cartItem = new CartItem()
+            });
+        }
+
+    } 
+
     deleteItem() {} // удаляет объект из массива списка товаров корзины
     changeQuantity() {} //изменяет количетво товара
     totalCostCart() {} // подсчитывает стоимость товаров в корзине
-    show() {} // корзина выпадает при нажатии на кнопку корзина
-    hide() {} // корзина скрывается
+    
+    
 }
 
 class CartItem{
-render() {} // возвращает строку с разметкой товара в корзине
-}
+    constructor(item, img = 'http://via.placeholder.com/100') {
+        this.title = item.product_name;
+        this.price = item.price;
+        this.id = item.id_product;
+        this.img = img;
+    }
+    
+    render() {
+        return `<div class="cart-item" data-id="${this.id}">
+        <img class="item-img" src="${this.img}" alt="${this.title}">
+        <h3>${this.title}</h3>
+        <p>${this.price}</p>
+    </div>`
+    } 
+    }
 
 
 
 let list = new ProductsList();
-list.render();
-list.totalCostProducts();
+let cartList = new CartList();
